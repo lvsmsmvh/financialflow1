5d7c303625ca27076ef561471c732a61e7e98b5c
lvsmsmch <iamajavagod@gmail.com>
2020-05-23 13:31:30 +0300

    добавлено sql , регистрация с sql, небольшие правки

---
+++ app/src/main/java/com/example/recycletest3/Constants.java
@@ -0,0 +1,7 @@
+package com.example.recycletest3;
+
+public class Constants {
+
+    public static final String ROOT_URL = "http://192.168.1.2/Android/v1/";
+    public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
+}
